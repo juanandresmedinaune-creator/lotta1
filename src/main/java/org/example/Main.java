ed3d32617b8ea759705351da778f1de126c52087
lottaxl <juanandresmedinaune@gmail.com>
2026-02-10 13:31:31 -0500

    POO

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,26 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        //crear persona desde el constructor vacio
+
+        persona p1 = new persona();
+
+        p1.setNombre("juan");
+        p1.setEdad(26);
+        p1.setGenero("Masculino");
+
+        //crear persona usando contructor con patrametros
+
+        persona p2 = new persona("Julian", "femenino", 23);
+
+        //SYSTEM OUT
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+
+
+
     }
 }
